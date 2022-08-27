181ac75bcf4119c1bdba7bb9ee58dce6ef541117
Vasyl Tarasyuk <vasfreelan@gmail.com>
2022-08-27 13:00:32 +0300

    task 3 is done

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,35 @@
+const form = document.querySelector('.form');
+const delayEl = document.querySelector('input[name=delay]');
+const stepEl = document.querySelector('input[name=step]');
+const amountEl = document.querySelector('input[name=amount]');
+
+form.addEventListener('submit', formSubmit);
+
+function formSubmit(event) {
+  event.preventDefault();
+  let delay = Number(delayEl.value);
+  for (let i = 1; i <= amountEl.value; i += 1) {
+    createPromise(i, delay)
+      .then(({ position, delay }) => {
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    delay += Number(stepEl.value);
+  }
+  form.reset();
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
